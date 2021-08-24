b74b9b5610e52fbaacc437236c4ea78a45137032
Wesley Prado <wprado1424@gmail.com>
2021-08-24 01:43:38 -0300

    refactor: separate bear-tile logic to another component

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,13 +1,18 @@
+import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 import searchBears from '@salesforce/apex/BearController.searchBears';
-export default class BearList extends LightningElement {
+
+export default class BearList extends NavigationMixin(LightningElement) {
 	searchTerm = '';
+
 	@wire(searchBears, {searchTerm: '$searchTerm'})
 	bears;
+
 	appResources = {
 		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
 	};
+
 	handleSearchTermChange(event) {
 
 		window.clearTimeout(this.delayTimeout);
@@ -17,7 +22,22 @@ export default class BearList extends LightningElement {
 			this.searchTerm = searchTerm;
 		}, 300);
 	}
+
 	get hasResults() {
 		return (this.bears.data.length > 0);
+	}
+	
+	handleBearView(event) {
+		// Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
 	}
 }
