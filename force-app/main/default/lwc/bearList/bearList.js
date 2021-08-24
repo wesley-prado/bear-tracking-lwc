e3d57d398a2704ddd7c0da7cb28483626a955b69
Wesley Prado <wprado1424@gmail.com>
2021-08-24 02:05:57 -0300

    feat: add bear map to home page

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,3 +1,5 @@
+import { publish, MessageContext} from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
 import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
@@ -5,9 +7,22 @@ import searchBears from '@salesforce/apex/BearController.searchBears';
 
 export default class BearList extends NavigationMixin(LightningElement) {
 	searchTerm = '';
-
-	@wire(searchBears, {searchTerm: '$searchTerm'})
 	bears;
+
+	@wire(MessageContext) messageContext;
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	loadBears(result){
+		this.bears = result;
+
+		if(result.data){
+			const message = {
+				bears: result.data
+			};
+
+			publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+		}
+	}
+	
 
 	appResources = {
 		bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
